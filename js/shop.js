5675b01357a1449a575e8d331e7ab9f4924fc5fb
RonniedeTal <166338751+RonniedeTal@users.noreply.github.com>
2025-02-12 22:14:19 +0100

    calculate totat

--- js/shop.js
+++ js/shop.js
@@ -1,69 +1,3 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
-
 
 // => Reminder, it's extremely important that you debug your code. 
 // ** It will save you a lot of time and frustration!
@@ -72,7 +6,8 @@ var products = [
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
-console.log(cart);
+
+
 
 var total = 0;
 
@@ -81,8 +16,6 @@ function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     const foundProduct = products.find( product => product.id == id )
     // 2. Add found product to the cart array
-  
-    
     cart.push( foundProduct )
     if(cart.includes(undefined)){
         cart.shift()
@@ -91,31 +24,33 @@ function buy(id) {
 
         total++
     }
-    console.log(total);
-    console.log(cart);
-    document.getElementById("count_product").innerHTML=total
+   document.getElementById("count_product").innerHTML=total
 }
 
-
-
-
-
-buy()
 
 // Exercise 2
 function cleanCart() {
-
+    const cartList = document.getElementById('cart_list');
+    cartList.innerHTML=""
+    //  cart.splice(0, cart.length)
+     
 }
 
 // Exercise 3
+
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
-    // for (let i = 0; i < cart.length; i++) {
-    //     total += cart[i].price * cart[i].amount;
-    //   }
-    //   console.log(total)
+   
+    let reduce=cart.reduce((acc, actual)=>acc+actual.price, 0)
+    let total=document.getElementById("total_price");
+    total.innerHTML=reduce
+    
+    
+   
+   
 }
-// calculateTotal()
+
+
 
 
 // Exercise 4
@@ -124,11 +59,25 @@ function applyPromotionsCart() {
 }
 
 // Exercise 5
+
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
-}
-
-
+    const cartList = document.getElementById('cart_list');
+        
+        const cartProducts = cart.map((eachProduct) => {
+          const cartArr = document.createElement('tr');
+          cartArr.innerHTML = `
+            <td>${eachProduct.name}</td>
+            <td>${eachProduct.price}</td>
+            
+          `;
+          return cartArr;
+        });
+  
+        cartList.append(...cartProducts);
+      }
+  
+      
 // ** Nivell II **
 
 // Exercise 7
@@ -137,5 +86,7 @@ function removeFromCart(id) {
 }
 
 function open_modal() {
+    cleanCart()
+    calculateTotal()
     printCart();
 }
