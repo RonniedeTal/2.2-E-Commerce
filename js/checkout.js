820cfe9ff7d9c4347baf794ca43da5e6beba7df9
RonniedeTal <166338751+RonniedeTal@users.noreply.github.com>
2025-02-19 17:57:29 +0100

    validations done

--- js/checkout.js
+++ js/checkout.js
@@ -1,70 +1,107 @@
-
 // Exercise 6
 function validate() {
-	var error ;
-	// Get the input fields
-	let submit=document.getElementById("btn")
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
-	let fAddress = document.getElementById("fAddress")
-	let fLastN = document.getElementById("fLastN")
-	let fPassword = document.getElementById("fPassword")
-	let fPhone = document.getElementById("fPhone")
+  // Get the input fields
+  let submit = document.getElementById("btn");
+  var fName = document.getElementById("fName");
+  var fEmail = document.getElementById("fEmail");
+  let fAddress = document.getElementById("fAddress");
+  let fLastN = document.getElementById("fLastN");
+  let fPassword = document.getElementById("fPassword");
+  let fPhone = document.getElementById("fPhone");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	let errorAddress = document.getElementById("errorAddress")
-	let errorLastN = document.getElementById("errorLastN")
-	let errorPassword = document.getElementById("errorPassword")
-	let errorPhone = document.getElementById("errorPhone")
-	
-	
-	// Validate fields entered by the user: name, phone, password, and email
+  // Get the error elements
+  var errorName = document.getElementById("errorName");
+  var errorEmail = document.getElementById("errorEmail");
+  let errorAddress = document.getElementById("errorAddress");
+  let errorLastN = document.getElementById("errorLastN");
+  let errorPassword = document.getElementById("errorPassword");
+  let errorPhone = document.getElementById("errorPhone");
 
-	const validateInputs=()=>{
-		const emailReg=/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi
-		
+  // Validate fields entered by the user: name, phone, password, and email
+  const emailReg = /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi;
+  const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+  const pattern = new RegExp("^[A-Z]+$", "i");
 
-		if(fName.value == ""||fName.value==null){
-			// error=errorName.value
-			errorName.innerHTML="holi"
-			fName.style.border = "1px solid red"
-			return false
-		}
-		if(!emailReg.test(fEmail.value)){
-			alert("ingrese valido")
-			fEmail.style.border = "1px solid red"
-			return false
-		}
-		return true
-	}
-	document.addEventListener("click", (e)=>{
-		// if(fName.value == ""||fName.value==null){
-		// 	let errorm=document.getElementById("invalid-feedback").value
-		// 	e.preventDefault()
-		// 	errorName.innerHtml=errorm
-		
-		// }
-		if(e.target===submit){
-			e.preventDefault()
-			validateInputs()
-		console.log("holi");
-		}
-		
-		
-	})
+  const validateInputs = () => {
+    if (
+      fName.value == "" ||
+      fName.value == null ||
+      fName.value.length < 3 ||
+      !pattern.test(fName.value)
+    ) {
+      fName.style.border = "1px solid red";
+      errorName.classList.add("invalid-feedback");
+      errorName.style.display = "block";
+    } else {
+      fName.style.border = "1px solid green";
+      errorName.classList.remove("invalid-feedback");
+      errorName.style.display = "none";
+    }
 
-	
+    if (!emailReg.test(fEmail.value)) {
+      fEmail.style.border = "1px solid red";
+      errorEmail.classList.add("invalid-feedback");
+      errorEmail.style.display = "block";
+    } else {
+      fEmail.style.border = "1px solid green";
+      errorEmail.classList.remove("invalid-feedback");
+      errorEmail.style.display = "none";
+    }
 
-	// if(fEmail.value == ""){
-	// 	error++;
-	// }
-	 
-	// if(error>0){
-	// 	alert("Error");
-	// }else{
-	// 	alert("OK");
-	// }
+    if (
+      fAddress.value == "" ||
+      fAddress.value == null ||
+      fAddress.value.length < 3
+    ) {
+      fAddress.style.border = "1px solid red";
+      errorAddress.classList.add("invalid-feedback");
+      errorAddress.style.display = "block";
+    } else {
+      fAddress.style.border = "1px solid green";
+      errorAddress.classList.remove("invalid-feedback");
+      errorAddress.style.display = "none";
+    }
 
+    if (
+      fLastN.value == "" ||
+      fLastN.value == null ||
+      fLastN.value.length < 3 ||
+      !pattern.test(fLastN.value)
+    ) {
+      fLastN.style.border = "1px solid red";
+      errorLastN.classList.add("invalid-feedback");
+      errorLastN.style.display = "block";
+    } else {
+      fLastN.style.border = "1px solid green";
+      errorLastN.classList.remove("invalid-feedback");
+      errorLastN.style.display = "none";
+    }
+
+    if (!passwordReg.test(fPassword.value)) {
+      fPassword.style.border = "1px solid red";
+      errorPassword.classList.add("invalid-feedback");
+      errorPassword.style.display = "block";
+    } else {
+      fPassword.style.border = "1px solid green";
+      errorPassword.classList.remove("invalid-feedback");
+      errorPassword.style.display = "none";
+    }
+
+    if (fPhone.value.length < 9 || fPhone.value.length > 9) {
+      fPhone.style.border = "1px solid red";
+      errorPhone.classList.add("invalid-feedback");
+      errorPhone.style.display = "block";
+    } else {
+      fPhone.style.border = "1px solid green";
+      errorPhone.classList.remove("invalid-feedback");
+      errorPhone.style.display = "none";
+    }
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target === submit) {
+      e.preventDefault();
+      validateInputs();
+    }
+  });
 }
--- js/shop.js
+++ js/shop.js
@@ -1,115 +1,110 @@
-
-// => Reminder, it's extremely important that you debug your code. 
+// => Reminder, it's extremely important that you debug your code.
 // ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
+// ** You'll understand the code better than with console.log(), and you'll also find errors faster.
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
 
-
-
-
 var total = 0;
-
 
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    const cartFinding= cart.find(product => product.id == id)
-        if (cartFinding==undefined){
-            const foundProduct = products.find( product => product.id == id )
-            cart.push( {...foundProduct, quantity:1} )
-           
-            }else{
-                cartFinding.quantity++
-            }    
-    // 2. Add found product to the cart array
-    // if(cart.includes(undefined)){ //al loro con esto me daba undefined--- parche     
-  if(cart.length!==0){
-        total++
-        document.getElementById("count_product").innerHTML=total
-    }
-    console.log(cart);//no remover hasta finalizar
-}
+  // 1. Loop for to the array products to get the item to add to cart
+  const cartFinding = cart.find((product) => product.id == id);
+  if (cartFinding == undefined) {
+    const foundProduct = products.find((product) => product.id == id);
+    cart.push({ ...foundProduct, quantity: 1 });
+  } else {
+    cartFinding.quantity++;
+  }
+  // 2. Add found product to the cart array
 
+  if (cart.length !== 0) {
+    total++;
+    document.getElementById("count_product").innerHTML = total;
+  }
+  console.log(cart); //no remover hasta finalizar
+}
 
 // Exercise 2
 function cleanCart() {
-    const cartList = document.getElementById('cart_list');
-    cartList.innerHTML=""
-    cart.splice(0, cart.length)
-    calculateTotal()
-    total= document.getElementById("count_product").innerHTML=0
+  const cartList = document.getElementById("cart_list");
+  cartList.innerHTML = "";
+  cart.splice(0, cart.length);
+  calculateTotal();
+  total = document.getElementById("count_product").innerHTML = 0;
 }
 
 // Exercise 3
 
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
-            let reduce=cart.reduce((acc, actual)=>acc+(actual.id==1?applyPromotionsCart():actual.price)*actual.quantity, 0)
-            let totalPrice=document.getElementById("total_price");
-             totalPrice.innerHTML=reduce.toFixed(2)
+  // Calculate total price of the cart using the "cartList" array
+  let reduce = cart.reduce(
+    (acc, actual) =>
+      acc +
+      (actual.id == 1 ? applyPromotionsCart() : actual.price) * actual.quantity,
+    0
+  );
+  let totalPrice = document.getElementById("total_price");
+  totalPrice.innerHTML = reduce.toFixed(2);
 }
-
 
 // Exercise 4
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
-    let applyPromo=0
-    let promo=cart.find(({id})=>id==1)
-    if(promo.id==1&&promo.quantity>=3){
-        applyPromo=parseFloat(promo.price-(promo.price*0.20))
-        // console.log(applyPromo);
-    }else{
-        applyPromo=parseFloat(promo.price)
-        // console.log(applyPromo);   
-    }
-    return applyPromo 
+  // Apply promotions to each item in the array "cart"
+  let applyPromo = 0;
+  let promo = cart.find(({ id }) => id == 1);
+  if (promo.id == 1 && promo.quantity >= 3) {
+    applyPromo = parseFloat(promo.price - promo.price * 0.2);
+  } else {
+    applyPromo = parseFloat(promo.price);
+  }
+  return applyPromo;
 }
-
 
 // Exercise 5
 
 function printCart() {
-    calculateTotal()
-    // Fill the shopping cart modal manipulating the shopping cart dom
-    const cartList = document.getElementById('cart_list');
-        cartList.innerHTML=""
-        const cartProducts=cart.map((eachProduct) => {
-          const cartArr=document.createElement('tr');
-          cartArr.innerHTML=`
+  calculateTotal();
+  // Fill the shopping cart modal manipulating the shopping cart dom
+  const cartList = document.getElementById("cart_list");
+  cartList.innerHTML = "";
+  const cartProducts = cart.map((eachProduct) => {
+    const cartArr = document.createElement("tr");
+    cartArr.innerHTML = `
             <td>${eachProduct.name}</td>
             <td>${eachProduct.price}</td>
-             <td>${eachProduct.quantity}<button onclick="removeFromCart(${eachProduct.id})">👎 </button></td>
-             <td>${eachProduct.id===1?applyPromotionsCart():eachProduct.price}</td>`;
-          return cartArr;
-        });
-        cartList.append(...cartProducts);
-      }
-  
-      
+             <td>${eachProduct.quantity}<button onclick="removeFromCart(${
+      eachProduct.id
+    })">👎 </button></td>
+             <td>${
+               eachProduct.id === 1 ? applyPromotionsCart() : eachProduct.price
+             }</td>`;
+    return cartArr;
+  });
+  cartList.append(...cartProducts);
+}
+
 // ** Nivell II **
 
 // Exercise 7
 function removeFromCart(id) {
-    const removeProduct= cart.findIndex(product=>product.id==id)
-    if(removeProduct!==-1){
-        const remove=cart[removeProduct]
-        if(remove.quantity>1){
-            remove.quantity--
-            total--
-        }else{
-            cart.splice(removeProduct, 1)
-            total--
-        }
-        document.getElementById("count_product").innerHTML = total;
+  const removeProduct = cart.findIndex((product) => product.id == id);
+  if (removeProduct !== -1) {
+    const remove = cart[removeProduct];
+    if (remove.quantity > 1) {
+      remove.quantity--;
+      total--;
+    } else {
+      cart.splice(removeProduct, 1);
+      total--;
     }
-    printCart()
+    document.getElementById("count_product").innerHTML = total;
+  }
+  printCart();
 }
 
 function open_modal() {
-    // applyPromotionsCart()
-    printCart();
+  printCart();
 }
-
